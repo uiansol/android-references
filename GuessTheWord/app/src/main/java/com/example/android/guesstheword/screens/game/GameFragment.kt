f7265dcd1baafdc3ce6d3317ef31dace5766a563
Uian Sol <sol.uian@gmail.com>
2021-01-14 21:31:00 -0300

    App GuessTheWord - Display results and use viewmodel factory

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,9 +21,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
@@ -52,10 +55,12 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
+        binding.endGameButton.setOnClickListener { onEndGame() }
+
         updateScoreText()
         updateWordText()
-        return binding.root
 
+        return binding.root
     }
 
     /** Methods for buttons presses **/
@@ -72,6 +77,20 @@ class GameFragment : Fragment() {
         updateScoreText()
     }
 
+    private fun onEndGame() {
+        gameFinished()
+    }
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
+    }
+
     /** Methods for updating the UI **/
 
     private fun updateWordText() {
@@ -81,4 +100,6 @@ class GameFragment : Fragment() {
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
     }
+
+
 }
